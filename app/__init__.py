5ec72296a9c79c033977f21e4072ff03da48f0d6
grandeelee <grandee.lee@gmail.com>
2025-10-20 19:29:33 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '37280d7558dcb27f84d64d6d91b8bb85ad2c0318b43fa894cea442ed154211e8'
 
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
     return app
 
-app = create_app()
--- app/app.py
+++
@@ -1,6 +0,0 @@
-from app import app
-from app.auth import bp as auth_bp
-from app.main import bp as main_bp
-
-app.register_blueprint(auth_bp, url_prefix='/auth')
-app.register_blueprint(main_bp)
